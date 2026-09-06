e4313cf0e2c95b637fdecf19e06d54506c2f05d5
antividaxr <sdquevedop@udistrital.edu.co>
2026-09-06 00:35:06 -0500

    18

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -218,9 +218,8 @@ public class Workshop {
 
     // Método que invierte una cadena
     public String invertirCadena(String cadena) {
-        // TODO: Implementar el método para invertir una cadena.
-        // Ejemplo: Si cadena = "Hello", el resultado debería ser "olleH".
-        return "";
+        if (cadena == null) return null;
+        return new StringBuilder(cadena).reverse().toString();
     }
 
     // Método que verifica si una cadena es un palíndromo
